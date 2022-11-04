252b9b0770c4d95a9fef518e28ef3fa5589a8847
Goomt <serosm93@gmail.com>
2022-11-04 22:20:02 +0900

    bootstrap

--- build/generated/sources/annotationProcessor/java/main/com/example/demo/mapper/MemberMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/example/demo/mapper/MemberMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-04T14:32:13+0900",
+    date = "2022-11-04T22:06:26+0900",
     comments = "version: 1.5.1.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 11.0.15 (Eclipse Adoptium)"
 )
 @Component
